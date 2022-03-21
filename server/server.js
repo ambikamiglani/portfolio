b507e6cc1d1f490d7f1aa8db71f397abd872552b
Ambika Miglani <ambika_miglani@brown.edu>
2022-03-21 19:37:23 -0400

    debugging deploy

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-// const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
-// app.use(express.static(publicPath));
-app.use(express.static(__dirname));
+app.use(express.static(publicPath));
+// app.use(express.static(__dirname));
 app.get("*", (req, res) => {
-  // res.sendFile(path.join(publicPath, 'index.html'));
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
+  //   res.sendFile(path.join(__dirname, "index.html"));
 });
 app.listen(port, () => {
   console.log("Server is up!");
