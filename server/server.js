2268c5e91867260cee7bcf2757e7ab63132fd42b
Ambika Miglani <ambika_miglani@brown.edu>
2022-03-22 19:40:11 -0400

    fixing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "build"); // could also be build
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 // app.use(express.static(__dirname));
 app.get("*", (req, res) => {
